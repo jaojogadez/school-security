c88dbc53f55fb6809850e7f481bfd6e9822c7589
João Pedro de Oliveira <jao07oliveiragames@gmail.com>
2025-04-08 12:20:23 -0300

    feat: adiciona funções para selecionar a aba e carregar tabela

--- src/modules/registers/table.js
+++ src/modules/registers/table.js
@@ -1,4 +1,31 @@
 import { dados } from "./dados.js";
+import { getCurrentTab, changeTab } from "./tabs.js";
 
-const tableHeader = document.querySelector("#tableHeader");
+// const tableHeader = document.querySelector("#tableHeader");
 const tableBody = document.querySelector("#tableBody");
+
+function loadTable() {
+  let currentTab = getCurrentTab();
+  try {
+    switch (currentTab) {
+      case "Visitantes":
+        console.log("Carregar tabela de visitantes");
+        break;
+      case "Funcionários":
+        console.log("Carregar tabela de funcionários");
+        break;
+      case "Alunos":
+        console.log("Carregar tabela de alunos");
+        break;
+      default:
+        console.log("Aconteceu algum erro.");
+        break;
+    }
+  } catch (error) {
+    console.log("Erro");
+    alert("Infelizemente não foi possível carregar a tabela.");
+  }
+}
+
+changeTab(loadTable);
+loadTable();
--- src/modules/registers/tabs.js
+++ src/modules/registers/tabs.js
@@ -1,5 +1,35 @@
+// Elementos HTML
 const navTabs = document.querySelector(".nav-tabs");
-navTabs.addEventListener("click", (event) => {
-    const tab = event.target;
-    console.log(tab)
-})
+const navLinks = document.querySelectorAll(".nav-tabs .nav-link");
+let currentTab = "Visitantes"; // Define uma aba padrão
+
+// Exporta a aba selecionada
+export function getCurrentTab() {
+  return currentTab;
+}
+
+// Exporta quando é aba alterada e recebe loadTable como argumento 
+export function changeTab(callback) {
+  navTabs.addEventListener("click", (event) => {
+    // Garante que somente a aba será clicada
+    if (!event.target.classList.contains("nav-link")) {
+      return;
+    }
+    currentTab = event.target.textContent;
+    getActiveTab();
+    // Executa função loadTable
+    if (callback) {
+      callback();
+    }
+  });
+}
+
+// Função para marcar a aba selecionada
+function getActiveTab() {
+  navLinks.forEach((link) => {
+    link.classList.remove("active");
+    if (link.textContent === currentTab) {
+      link.classList.add("active");
+    }
+  });
+}
